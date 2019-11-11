9bcd3bc6c33e99768c95582c83864a0b191e2df8
tim ichiba <ichibat@gmail.com>
2019-11-11 12:08:54 +0900

    final

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>jokes</div>
+  <div>
+    <SearchJokes v-on:search-text="searchText"/>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+
+  </div>
 </template>
 
 <script>
 
 import axios from "axios";
+import Joke from '../../components/Joke'
+import SearchJokes from '../../components/SearchJokes'
 
 export default {
+  components: {
+    Joke,
+    SearchJokes
+  },
   data() {
     return {
       jokes: []
@@ -21,12 +31,26 @@ export default {
 
       try {
           const res = await axios.get('https://icanhazdadjoke.com/search', config);
-          console.log(res.data);
+          this.jokes = res.data.results;
       } catch (error) {
         console.log(err)
       }
+  },
+  methods: {
+    async searchText(text){
+const config = {
+      headers: {
+        'Accept': 'application/json'
+      }
+      }
 
-
+      try {
+          const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config);
+          this.jokes = res.data.results;
+      } catch (error) {
+        console.log(err)
+      }
+    }
   },
   head(){
     return {
